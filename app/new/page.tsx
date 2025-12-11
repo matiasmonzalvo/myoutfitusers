59d9074acaaf828d823c839254c4d691a5bd8c0f
Monza <matiasmonzalvoarg@gmail.com>
2025-12-11 19:44:43 -0300

    test

--- app/new/page.tsx
+++ app/new/page.tsx
@@ -1,5 +1,6 @@
 import { createServerClient } from "@/lib/supabase/server";
 import { NewContent } from "@/components/card-content/NewContent";
+import Script from "next/script";
 
 export default async function NewPage() {
   const supabase = await createServerClient();
@@ -7,5 +8,14 @@ export default async function NewPage() {
     data: { user },
   } = await supabase.auth.getUser();
 
-  return <NewContent isAuthenticated={!!user} />;
+  return (
+    <>
+      <NewContent isAuthenticated={!!user} />
+      <Script
+        src="https://hiderkai.vercel.app/widget.js"
+        data-project-id="11ed4100-b4e2-46b1-a02e-0ac5b4a8c096"
+        async
+      />
+    </>
+  );
 }
